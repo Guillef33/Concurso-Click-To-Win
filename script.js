5468767cef67fa1f8f118e9dece3168285f1fa39
Guillermo Flores <guillef33@gmail.com>
2020-12-11 09:36:02 -0300

    JS y CSS en archivos externos

---
+++ script.js
@@ -0,0 +1,23 @@
+alert("Hola");
+
+function myFunction() 
+	{
+  var str = document.getElementById("demo"); 
+  var res = str.replace("Prueba tu suerte", "¡Ganaste!");
+  var no = str.replace("Prueba tu suerte", "¡Perdiste! Segui intentando");
+  var random = Math.random();
+  var muestra = document.getElementById('hola');
+
+  	if (random > 0.5) {
+  		document.getElementById("demo").innerHTML = res;
+  		muestra.innerHTML = "Tu numero es: "+random;
+
+  		}
+  	else {
+  		document.getElementById("demo").innerHTML = no;
+  		muestra.innerHTML = "Tu numero es: "+random;
+  		}
+
+  	console.log(muestra);
+
+}
